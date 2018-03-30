4b267568a652d02fd875ba3972d29cea88e661a5
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2018-03-30 21:28:19 +0300

    Updated intro section for mobile and desktop.

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+var screenHeight = $(window).height();
+
+
+$(function () {
+  $('.navbar-nav li a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+
+
+});
+
+$(window).on("scroll", function () {
+  var scTop = $(window).scrollTop(); 
+
+  console.log(scTop);
+  if (scTop > screenHeight) {    
+    $(".navbar").addClass("with-bg");
+  } else {
+    $(".navbar").removeClass("with-bg");
+  }
+})
