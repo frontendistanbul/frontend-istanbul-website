387b7a02fce744543980c95b39f2dda7272a4238
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2018-04-10 11:08:09 +0300

    Changed validation message display.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,21 +53,11 @@ $(window).on('scroll', function() {
 var request;
 
 // Bind to the submit event of our form
-var sEmail = $("#subscribe-form #email").val();
-
-$("#subscribe-form #email").on("blur", function () {  
-  sEmail = $("#subscribe-form #email").val();
-  if (validateEmail(sEmail)) {
-    $("#subscribe-form").removeClass("error");
-   } else {
-    $("#subscribe-form").addClass("error");   
-    $("#emailHelp").html("Lütfen geçerli bir e-posta adresi yazınız.");
-   }
-})
-
 
 $("#subscribe-form").submit(function (event) {
+  var sEmail = $("#subscribe-form #email").val();
   // Prevent default posting of form
+  $("#subscribe-form").removeClass("error");   
   event.preventDefault();
   if (validateEmail(sEmail)) {
       if (request) {
@@ -93,10 +83,22 @@ $("#subscribe-form").submit(function (event) {
       request.always(function () {
           // Reenable the inputs
         $("#emailHelp").html("Tebrikler! E-posta adresinizi kaydedildi.").fadeIn();
+        
+        $inputs.prop("disabled", false);
+        console.log("DONE");
+        
+        setTimeout( function() {
+          $("#emailHelp").fadeOut();
+        }, 3000);
           
       });
-  } 
+  } else {
+    $("#subscribe-form").addClass("error");   
+    $("#emailHelp").html("Lütfen geçerli bir e-posta adresi yazınız.");
+  }
 });
+
+
 
 
 function validateEmail(sEmail) {
